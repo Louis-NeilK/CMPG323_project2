b7085b83aa374b694ebafe6252443c113bf93ad3
Louis-Neil Korsten <louisneilkorsten@gmail.com>
2021-11-18 19:34:35 +0200

    heroku set up

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("index")
+})
 
 let port = process.env.PORT;
 if (port == null || port == "") {
 	port = 3000;
 }
-server.listen(port, function () {
+app.listen(port, function () {
 	console.log("Server started successfully.");
 });
