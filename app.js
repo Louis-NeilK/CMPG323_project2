8cbd11f357b7f6b6917a428dab0b5ded5e9bf54e
Louis-Neil Korsten <louisneilkorsten@gmail.com>
2021-11-18 20:31:13 +0200

    Set up mongo and configure database connection

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect(
+	"mongodb+srv://admin-louis:test123@cluster0.9pokq.mongodb.net/fotoshareDB",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+);
 
 app.get("/", function(req, res){
     res.render("index")
